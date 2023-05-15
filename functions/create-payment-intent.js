99ce909f38f3cc28029fd7dab4cee4848613e097
DragotaIV <dragotaiv@gmail.com>
2023-05-16 00:27:45 +0300

    first

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -3,13 +3,13 @@ dotenv.config()
 const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY)
 exports.handler = async function (event, context) {
 console.log(event)
-  const { cart, shipping_fee, total_amount } = JSON.parse(event.body)
+  const { cart, shipping_free, total_amount } = JSON.parse(event.body)
 
   const calculateOrderAmount = () => {
     // Replace this constant with a calculation of the order's amount
     // Calculate the order total on the server to prevent
     // people from directly manipulating the amount on the client
-    return shipping_fee + total_amount
+    return shipping_free + total_amount
   }
   try {
     // Create a PaymentIntent with the order amount and currency
